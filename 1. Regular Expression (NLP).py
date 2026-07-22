473f7f308c32e78847165d3529563b0c45c8ec64
Mohaiyadeen2006 <mohamedmohaiyadeen28122006@gmail.com>
2026-07-22 13:09:08 +0530

    Add files via upload

---
+++ 1. Regular Expression (NLP).py
@@ -0,0 +1,12 @@
+import re
+
+text = "My phone number is 9876543210"
+
+pattern = r"\d{10}"
+
+result = re.search(pattern, text)
+
+if result:
+    print("Phone Number Found:", result.group())
+else:
+    print("Not Found")
